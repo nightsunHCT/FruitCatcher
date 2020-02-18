35190f738ea761587d9944e3b9fb4b2ad10b93b2
arthurscreative <arthur.silva@rutgers.edu>
2020-02-18 17:27:59 -0500

    Created The tree movment script
    can be ajusted later dropps oranges because im alergic to apples

---
+++ Assets/Scripts/TreeMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeMovement : MonoBehaviour
+{
+    //orange
+    public GameObject orangeprefab;
+    public float speed = 1f; //spped that tree moves
+    public float leftAndRightEdge = 10f; // distnace where tree turns around
+    public float chanceToChangeDirections = 0.02f; //chance tree changes direction
+    public float secondsBetweenOrangeDrops = 1f; //rate at which apples will be instantaited
+
+    void Start()
+    {
+        // Dropping oranges every second
+        InvokeRepeating("DropOrange", 2f, secondsBetweenOrangeDrops);
+    }
+
+    void DropOrange()
+    {
+        GameObject orange = Instantiate(orangeprefab) as GameObject;
+        orange.transform.position = transform.position;
+    }
+    void Update()
+    {
+        //basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        //change of direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); //move right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); // move left
+        }
+
+    }
+    void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1; //change directions
+        }
+    }
+
+
+}
+
+
