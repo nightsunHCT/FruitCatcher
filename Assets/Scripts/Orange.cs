b1632a456ada6019aec63a53da1fa226d65eccb9
Helena <nightsunHCT@users.noreply.github.com>
2020-02-19 02:33:40 -0500

    Physics collider remove + destroy orange

---
+++ Assets/Scripts/Orange.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orange : MonoBehaviour
+{
+    public static float bottomY = -8f;
+
+    void Update()
+    {
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject, 1f);
+        }
+    }
+}
