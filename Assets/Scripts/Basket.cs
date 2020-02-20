bdf5585cae4ad099eb61f247b1d2760726f55789
arthurscreative <arthur.silva@rutgers.edu>
2020-02-19 20:33:40 -0500

    Added stuff
    added to basket script
    created high score script
    created UI

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
@@ -13,7 +14,14 @@ public class Basket : MonoBehaviour
             Destroy(this.gameObject, 1f);
         }
     }
+    public Text scoreGt;
 
+    void Start()
+    {
+        GameObject scoreGo = GameObject.Find("Score");
+        scoreGt = scoreGo.GetComponent<Text>();
+        scoreGt.text = "0";
+    }
     void OnCollisionEnter (Collision coll)
     {
         // find what hit the basket
@@ -21,7 +29,22 @@ public class Basket : MonoBehaviour
         if (collideWith.tag == "Orange")
         {
             Destroy(collideWith);
+
         }
+        int score = int.Parse(scoreGt.text);
+        score += 1;
+        scoreGt.text = score.ToString();
+
+        if (score > HighScore.score)
+        {
+            HighScore.score = score;
+        }
+
     }
+    
+
+   
+       
+
 
 }
